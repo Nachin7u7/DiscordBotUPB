d8ba7fe99c4faba26b5ec62b4e2e06edef017f28
Ignacio <ignacio.valencia.sauma@gmail.com>
2024-04-05 20:28:50 -0400

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+# UPB Bot para discord
+## hecho por R. I. Valencia
+Actualmente en desasrrollo para enviar mensajes QA por discord
--- mi_bot.py
+++ mi_bot.py
@@ -1,58 +1,79 @@
 import discord
 from discord.ext import commands
+import requests
 
-TOKEN = 'Agregar token de bot aqui'
+TOKEN = 'tu_token_aquí'
 
 intents = discord.Intents.default()
 intents.messages = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
+async def consulta_ninja_cat_api():
+    url = 'https://catfact.ninja/fact?max_length=1000'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data['fact']
+    else:
+        return 'No se pudo obtener un hecho de gatos en este momento. Inténtalo de nuevo más tarde.'
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} ha iniciado sesión')
 
+
 @bot.command()
 async def enviar_mensaje(ctx, usuario: discord.User, *, mensaje: str):
     await usuario.send(mensaje)
-    await ctx.send(f'Se ha enviado un mensaje a {usuario.name}')
+    embed = discord.Embed(title="Mensaje Enviado",
+                          description=f'Se ha enviado un mensaje a {usuario.mention}', color=0x00ff00)
+    if bot.user.avatar:
+        embed.set_thumbnail(url=bot.user.avatar.url)
+    await ctx.send(embed=embed)
 
 
 @bot.command()
 async def verificar(ctx):
     if discord.utils.get(ctx.author.roles, name='Admin'):
-        await ctx.send('¡Eres un administrador!')
+        embed = discord.Embed(
+            title="Verificación", description="¡Eres un administrador! :crown:", color=0x00ff00)
     else:
-        await ctx.send('No eres un administrador.')
+        embed = discord.Embed(
+            title="Verificación", description="No eres un administrador.", color=0xff0000)
+    if bot.user.avatar:
+        embed.set_thumbnail(url=bot.user.avatar.url)
+    await ctx.send(embed=embed)
 
 
 @bot.command()
 async def anunciar(ctx, canal: discord.TextChannel, *, mensaje: str):
-    await canal.send(mensaje)
-    await ctx.send(f'Anuncio enviado al canal {canal.mention}')
+    await canal.send(embed=discord.Embed(title="Anuncio", description=mensaje, color=0x00ff00))
+    embed = discord.Embed(
+        title="Anuncio", description=f'Anuncio enviado al canal {canal.mention}', color=0x00ff00)
+    if bot.user.avatar:
+        embed.set_thumbnail(url=bot.user.avatar.url)
+    await ctx.send(embed=embed)
+
 
 @bot.command()
 async def consulta_api(ctx):
-    # TODO Realizar la consulta a la API aquí
-    await ctx.send('Resultado de la consulta a la API')
+    fact = await consulta_ninja_cat_api()
+    embed = discord.Embed(
+        title="Consulta API", description=f'**Hecho de gatos:**\n\n*{fact}*', color=0x00ff00)
+    if bot.user.avatar:
+        embed.set_thumbnail(url=bot.user.avatar.url)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def fact_gato(ctx):
+    fact = await consulta_ninja_cat_api()
+    embed = discord.Embed(title="Hecho de gatos",
+                          description=f'\n\n*{fact}*', color=0x00ff00)
+    if bot.user.avatar:
+        embed.set_thumbnail(url=bot.user.avatar.url)
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
-
-
-# Función para probar enviar un mensaje privado
-# async def probar_enviar_mensaje():
-#     # Reemplaza usuario_id con el ID de 'nachin7u7'
-#     print("enviando mensaje...")
-#     usuario = await bot.fetch_user("nachin7u7#0657")
-#     await bot.invoke(await bot.get_context(discord.Message(content=f"!enviar_mensaje {usuario.mention} Hola, esto es un mensaje privado para ti.")))
-
-# Función para probar hacer un anuncio
-
-
-# async def probar_anunciar():
-#     print("enviando anuncio...")
-#     await bot.invoke(await bot.get_context(discord.Message(content="!anunciar Hola! Soy el bot de prueba y esto es una prueba.")))
-
-# Llama a las funciones de prueba
-# bot.loop.create_task(probar_enviar_mensaje())
-# bot.loop.create_task(probar_anunciar())
