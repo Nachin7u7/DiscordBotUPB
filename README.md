c688bbaa4613adb005d28553259fe399c244bc80
Ignacio <ignacio.valencia.sauma@gmail.com>
2024-04-22 00:59:56 -0400

    mensajes de saldos, diamantes y ayuda

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # UPB Bot para discord
 ## hecho por R. I. Valencia
-Actualmente en desasrrollo para enviar mensajes QA por discord en el canal de texto especial para este bot
+Actualmente en desasrrollo para enviar mensajes al servidor de discord de los estudiantes de la UPB para administrar saldos, diamantes y con eso estimar puntaje y demas
--- mi_bot.py
+++ mi_bot.py
@@ -1,96 +1,126 @@
 import discord
-from discord.ext import commands
+import os
 import requests
-import asyncio
-
-TOKEN = 'tu_token_de_discord'
+from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.messages = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='b!', intents=intents)
+my_secret = os.environ['MyToken']
+api_base_url = "https://grades-microservice.vercel.app/api/v1"
 
-# Función para obtener el webhook de un canal específico
-async def get_webhook(channel_id):
-    channel = bot.get_channel(channel_id)
-    if channel:
-        webhooks = await channel.webhooks()
-        if webhooks:
-            return webhooks[0]  # Devuelve el primer webhook en el canal si ya existe uno
-        else:
-            return await channel.create_webhook(name="Anuncio Webhook")  # Crea un nuevo webhook si no hay ninguno
-    else:
-        return None
 
-async def consulta_ninja_cat_api():
-    url = 'https://catfact.ninja/fact?max_length=1000'
-    response = requests.get(url)
-    if response.status_code == 200:
+async def get_payment_details(enrollment_code):
+    api_url = f"{api_base_url}/paymentDetail/{enrollment_code}"
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.get(api_url, headers=headers)
         data = response.json()
-        return data['fact']
+        if response.status_code == 200:
+            coins = float(data['data']['coins'])
+            diamonds = float(data['data']['diamond'])
+            return coins, diamonds
+        else:
+            return None, None
+    except Exception as e:
+        print(f'Error al conectar con la API: {str(e)}')
+        return None, None
+
+
+@bot.command()
+async def ayuda(ctx):
+    embed = discord.Embed(
+        title="¡Bienvenido soy UPB Server Bot!",
+        description="Aquí tienes una lista de comandos disponibles:",
+        color=0x00ff00  # Color verde
+    )
+    embed.add_field(name="b!saldo [código de estudiante UPB]",
+                    value="Consulta el saldo de monedas y diamantes.")
+    embed.add_field(name="b!coins [cantidad] [código de estudiante UPB]",
+                    value="Añade monedas a la cuenta.(Solo para personal autorizado)")
+    embed.add_field(name="b!diamonds [cantidad] [código de estudiante UPB]",
+                    value="Añade diamantes a la cuenta.(Solo para personal autorizado)")
+    embed.add_field(name="b!ayuda", value="Muestra este mensaje de ayuda.")
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def saldo(ctx, enrollment_code: int):
+    coins, diamonds = await get_payment_details(enrollment_code)
+    if coins is not None and diamonds is not None:
+        embed = discord.Embed(
+            title="Saldo",
+            description=f"Saldo actual: {coins} monedas, {diamonds} diamantes",
+            color=0x00ff00  # Color verde
+        )
+        await ctx.send(embed=embed)
     else:
-        return 'No se pudo obtener un hecho de gatos en este momento. Inténtalo de nuevo más tarde.'
+        await ctx.send('Error al obtener el saldo.')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} ha iniciado sesión')
 
 @bot.command()
-async def enviar_mensaje(ctx, usuario: discord.User, *, mensaje: str):
-    await usuario.send(mensaje)
-    embed = discord.Embed(title="Mensaje Enviado", description=f'Se ha enviado un mensaje a {usuario.mention}', color=0x00ff00)
-    if bot.user.avatar:
-        embed.set_thumbnail(url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def verificar(ctx):
-    if discord.utils.get(ctx.author.roles, name='Admin'):
-        embed = discord.Embed(title="Verificación", description="¡Eres un administrador! :crown:", color=0x00ff00)
+@commands.has_any_role('Lead', 'Senior Admin')
+async def coins(ctx, amount: float, enrollment_code: int):
+    current_coins, current_diamonds = await get_payment_details(enrollment_code)
+    if current_coins is not None:
+        new_coins = current_coins + amount
+        api_url = f"{api_base_url}/paymentDetail/{enrollment_code}"
+        headers = {"Content-Type": "application/json"}
+        body = {"coins": new_coins}
+        try:
+            response = requests.put(api_url, json=body, headers=headers)
+            data = response.json()
+            if response.status_code == 200:
+                embed = discord.Embed(
+                    title="Monedas añadidas correctamente",
+                    description=f'Se han añadido {amount} monedas correctamente. Nuevo saldo: {new_coins} monedas.',
+                    color=0x00ff00  # Color verde
+                )
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(
+                    title="Error al añadir monedas",
+                    description=f'Error: {data["message"]}',
+                    color=0xff0000  # Color rojo
+                )
+                await ctx.send(embed=embed)
+        except Exception as e:
+            print(f'Error al conectar con la API: {str(e)}')
+            await ctx.send('Error al conectar con la API.')
     else:
-        embed = discord.Embed(title="Verificación", description="No eres un administrador.", color=0xff0000)
-    if bot.user.avatar:
-        embed.set_thumbnail(url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
+        await ctx.send('Error al obtener el saldo actual.')
+
 
 @bot.command()
-async def anunciar(ctx, canal: discord.TextChannel, *, mensaje: str):
-    await canal.send(embed=discord.Embed(title="Anuncio", description=mensaje, color=0x00ff00))
-    embed = discord.Embed(title="Anuncio", description=f'Anuncio enviado al canal {canal.mention}', color=0x00ff00)
-    if bot.user.avatar:
-        embed.set_thumbnail(url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def consulta_api(ctx):
-    fact = await consulta_ninja_cat_api()
-    embed = discord.Embed(title="Consulta API", description=f'**Hecho de gatos:**\n\n*{fact}*', color=0x00ff00)
-    if bot.user.avatar:
-        embed.set_thumbnail(url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def fact_gato(ctx):
-    fact = await consulta_ninja_cat_api()
-    embed = discord.Embed(title="Hecho de gatos", description=f'\n\n*{fact}*', color=0x00ff00)
-    if bot.user.avatar:
-        embed.set_thumbnail(url=bot.user.avatar.url)
-    await ctx.send(embed=embed)
-
-async def announce_with_webhook(webhook, mensaje):
-    await webhook.send(embed=discord.Embed(title="Anuncio", description=mensaje, color=0x00ff00))
-
-async def call_anunciar():
-    # Aquí necesitas el ID del canal en el que quieres realizar el anuncio
-    canal_id = 1224440282058199222
-    mensaje = "Este es un anuncio realizado desde el bot usando un webhook."
-    webhook = await get_webhook(canal_id)
-    if webhook:
-        await announce_with_webhook(webhook, mensaje)
+@commands.has_any_role('Lead', 'Senior Admin')
+async def diamonds(ctx, amount: float, enrollment_code: int):
+    current_coins, current_diamonds = await get_payment_details(enrollment_code)
+    if current_diamonds is not None:
+        new_diamonds = current_diamonds + amount
+        api_url = f"{api_base_url}/paymentDetail/{enrollment_code}"
+        headers = {"Content-Type": "application/json"}
+        body = {"diamond": new_diamonds}
+        try:
+            response = requests.put(api_url, json=body, headers=headers)
+            data = response.json()
+            if response.status_code == 200:
+                embed = discord.Embed(
+                    title="Diamantes añadidos correctamente",
+                    description=f'Se han añadido {amount} diamantes correctamente. Nuevo saldo: {new_diamonds} diamantes.',
+                    color=0x00ff00  # Color verde
+                )
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(
+                    title="Error al añadir diamantes",
+                    description=f'Error: {data["message"]}',
+                    color=0xff0000  # Color rojo
+                )
+                await ctx.send(embed=embed)
+        except Exception as e:
+            print(f'Error al conectar con la API: {str(e)}')
+            await ctx.send('Error al conectar con la API.')
     else:
-        print("No se pudo encontrar el webhook o crear uno nuevo.")
+        await ctx.send('Error al obtener el saldo actual.')
 
-async def main():
-    await bot.start(TOKEN)
-    await call_anunciar()
-
-asyncio.run(main())
+bot.run(my_secret)
